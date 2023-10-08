6cfa84f12b8f0aaa335cd72f091f449df5f67dd9
Kye Choong <kyecdesign@gmail.com>
2023-10-09 01:16:11 +0900

    Update 1

--- js/main.js
+++ js/main.js
@@ -52,13 +52,42 @@ function updateCell(cell, index) {
 }
 
 function changePlyaer() {
-
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 }
 
 function checkWinner() {
+    let roundWon = false;
 
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if (cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if (cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if (roundWon) {
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    } else if (!options.includes("")) {
+        statusText.textContent = `Draw!`;
+        running = false;
+    } else {
+        changePlyaer();
+    }
 }
 
 function restartGame() {
-
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
 }
